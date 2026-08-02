c36f62a98143dd4b3046ac00d29dd78847c076b5
Jude <133681884+YlPorts@users.noreply.github.com>
2026-08-01 21:29:21 -0500

    Fix InstanceDock cleanup for Godot 4.7.1 headless imports

--- addons/InstanceDock/InstanceDockPlugin.gd
+++ addons/InstanceDock/InstanceDockPlugin.gd
@@ -11,22 +11,27 @@ func _enter_tree():
 	paint_mode = dock.paint_mode
 
 func _exit_tree():
-	remove_control_from_docks(dock)
-	dock.free()
+	if is_instance_valid(dock):
+		remove_control_from_bottom_panel(dock)
+		dock.queue_free()
 
 func _handles(object: Object) -> bool:
-	return paint_mode.enabled and object is CanvasItem
+	return is_instance_valid(paint_mode) and paint_mode.enabled and object is CanvasItem
 
 func _edit(object: Object) -> void:
-	paint_mode.edited_node = object
+	if is_instance_valid(paint_mode):
+		paint_mode.edited_node = object
 
 func _forward_canvas_gui_input(event: InputEvent) -> bool:
-	return paint_mode.paint_input(event)
+	return is_instance_valid(paint_mode) and paint_mode.paint_input(event)
 
 func _forward_canvas_draw_over_viewport(viewport_control: Control) -> void:
-	paint_mode.paint_draw(viewport_control)
+	if is_instance_valid(paint_mode):
+		paint_mode.paint_draw(viewport_control)
 
 func _get_window_layout(configuration: ConfigFile) -> void:
+	if not is_instance_valid(dock):
+		return
 	var tabs: TabBar = dock.tabs
 	tabs.set_tab_metadata(tabs.current_tab, dock.scroll.scroll_vertical)
 	
@@ -34,6 +39,8 @@ func _get_window_layout(configuration: ConfigFile) -> void:
 		configuration.set_value("InstanceDock", "tab_%d_scroll" % i, tabs.get_tab_metadata(i))
 
 func _set_window_layout(configuration: ConfigFile) -> void:
+	if not is_instance_valid(dock):
+		return
 	var tabs: TabBar = dock.tabs
 	for i in tabs.tab_count:
 		tabs.set_tab_metadata(i, configuration.get_value("InstanceDock", "tab_%d_scroll" % i, 0))
